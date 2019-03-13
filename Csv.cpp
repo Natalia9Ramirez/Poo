75d7979a1d0112131a74124302b7489bb27132d1
Natalia Ramirez Almeida <ramirezanatalia19@gmail.com>
2019-03-13 11:50:33 -0500

    Texto plano y generación csv

--- Csv.cpp
+++ Csv.cpp
@@ -16,44 +16,76 @@ class extraer{
 	  }	   
 	  string generar()
 		{
-			string dato="";
-			string salida = "";
-			string cc="", nombre="", cargo="";
-			int band=0; int coma=0;
+			string dato="", salida="", cc="", nombre="", cargo="",sueldo="",anios="";
+			int coma=0,sueldoNeto=0, descuento=0, auxilio=0;
 			
 			for(int i=0; i<linea.length(); i++)
 			{			
 				if(linea.at(i)==','){
-					coma++;
-					
+					coma++;					
 					switch(coma)
 					{
 						case 1:
 							cc=dato;
-							printf("%s\n", dato.c_str());
+							printf("Documento: %s\n",dato.c_str());
 							dato="";
 							i++;
 							break;
+							
 						case 2:
 							nombre=dato;
-							printf("%s\n",nombre.c_str());
-						break;						
+							printf("Nombre: %s\n",nombre.c_str());
+							dato="";
+							i++;
+							break;
 							
-					}
-					
-					/*for	(int j=0;j<i;j++)
-					{	band= i;
-						cc=cc+linea.at(j);
-					}*/
-				
+						case 3:
+							cargo=dato;
+							printf("Cargo: %s\n",cargo.c_str());
+							dato="";
+							i++;
+							break;
+							
+						case 4:
+							sueldo=dato;
+							printf("Sueldo base: %s\n",sueldo.c_str());
+							descuento = (toInt(sueldo)*8)/100;
+							sueldoNeto = toInt(sueldo) - descuento;
+							printf("Valor descuento: %d\n", descuento);
+							printf("Sueldo neto: %d\n", sueldoNeto);
+							dato="";
+							i++;
+							break;
+							
+						case 5:
+							anios=dato;
+							printf("Antiguedad: %s\n",anios.c_str());
+							if(toInt(anios)>=3)
+							{
+								auxilio = (toInt(sueldo)*8)/100;
+								sueldoNeto = toInt(sueldo) + auxilio;
+								printf("Auxilio por antiguedad: %d\n", auxilio);
+								printf("Sueldo neto: %d\n", sueldoNeto);
+							}
+							else
+							{
+								printf("No aplica auxilio por antiguedad\n");
+							}
+							dato="";
+							i++;
+							break;
+						break;					
+							
+					}		
 				}else
 				{
 					dato=dato+linea.at(i);
 				}
+				
+				
 		}
-			
-			/*salida= cc;
-			return (salida);*/
+		salida=cc+";"+cargo+";"+ toString(descuento)+";"+toString(auxilio)+";"+toString(sueldoNeto);
+		return(salida);
 		}
 	
 		int toInt(string valor)
@@ -65,6 +97,15 @@ class extraer{
 			ss >> i;
 			return (i);
 		}
+		
+		string toString(int valor)
+		{
+			string s1;
+			ostringstream ss;
+			ss << valor;
+			s1=ss.str();
+			return (s1);
+		}
 };
 
 class procesar 
@@ -72,20 +113,30 @@ class procesar
 	private: 
 	public:
 		procesar(){}
-		void procesarLinea()
+		void generarSalida(){
+		ofstream destino;
+		destino.open ("c:\\Prueba\\salida.csv");
+		
+		ifstream origen("c:\\Prueba\\linea.txt");
+		string linea;
+		string salida=""; 
+		
+		destino<<"DOCUMENTO;CARGO;DESCUENTO SALUD Y PENSION;AUXILIO ANTIGUEDAD;SUELDO NETO\n";
+		
+		while (getline(origen, linea))
 		{
-			string linea ="";
-			ifstream archivo("C:\\Prueba/linea.txt");
-			string salida ="";
-			while(getline(archivo, linea)){
-				extraer e(linea);
-				salida = e.generar();
-				printf("%s \n", salida.c_str());  
-			}	
+		   extraer e(linea);
+		   salida=e.generar();
+		   destino <<salida+"\n";	
+			
 		}
+		origen.close();
+		destino.close(); 
+		printf("Proceso finalizado!");
+  }
 };
 
 int main(int argc, char** argv) {
 	procesar p; 
-	p.procesarLinea(); 
+	p.generarSalida(); 
 	}
